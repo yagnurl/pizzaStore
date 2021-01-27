53e8ac029d583ea6c1b895bf5d8435c1183b65d2
Yağnur Tetikcan <50737107+yagnurl@users.noreply.github.com>
2021-01-28 01:34:45 +0300

    Add files via upload

---
+++ src/components/Data.js
@@ -0,0 +1,23 @@
+
+import product1 from "../images/pizzas/product1.jpg"
+import product2 from "../images/pizzas/product2.jpg"
+import product3 from "../images/pizzas/product3.jpg"
+import product4 from "../images/pizzas/product4.jpg"
+import product6 from "../images/pizzas/product6.jpg"
+import product7 from "../images/pizzas/product7.jpg"
+import product8 from "../images/pizzas/product8.jpg"
+import product9 from "../images/pizzas/product9.jpg"
+import product10 from "../images/pizzas/product10.jpg"
+import product11 from "../images/pizzas/product11.jpg"
+import product12 from "../images/pizzas/product12.jpg"
+import product13 from "../images/pizzas/product13.jpg"
+import product14 from "../images/pizzas/product14.jpg"
+import product15 from "../images/pizzas/product15.jpg"
+import product16 from "../images/pizzas/product16.jpg"
+
+
+const datas = [product1, product2, product3, product4, product6, product7, product8, product9, product10, product11, product12, product13, product14, product15, product16,]
+
+
+
+export default datas 
